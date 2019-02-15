0886f0896eebf0719bb38e51b6973a133dd33d70
雷霆 <leiting@leitingmacbook-pro.local>
2019-02-15 14:45:44 +0800

    init

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,48 @@
 import { Component } from '@angular/core';
+import { isDefined } from '@angular/compiler/src/util';
+import { callCordovaPlugin } from '@ionic-native/core/decorators/common';
+import { from } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+  toNative(){
+    cordova.exec(this.success,this.fail,"MyPlugin","toNative",["1234"])
+  }
+
+   success(str:String) {
+     alert("receive data:"+str)
+    
+  }
+
+   fail(params:String) {
+    
+  }
+  getPicture(){
+    // var cameraOptions= {
+    //   quality : 75,
+    //   destinationType : Camera.DestinationType.DATA_URL,
+    //   sourceType : Camera.PictureSourceType.SAVEDPHOTOALBUM,    //相册类型
+    //   allowEdit : true,
+    //   encodingType : Camera.EncodingType.JPEG,
+    //   targetWdith : 100,
+    //   targetHeight : 100,}
+      // navigator.camera.getPicture(this.onCameraSuccess,this.onCamerafail,null)
+
+      alert(device.uuid+"")
+
+  }
+
+  onCameraSuccess(url){
+
+  }
+  onCamerafail(url){
+
+  }
+
+}
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import {InfoPage} from'../info/info.page'
+import { from } from 'rxjs';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+export class Tab3Page {
+  constructor(public navCtrl: NavController) {
+  }
+  toInfo(){
+    this.navCtrl.navigateForward("info")
+  }
+}
